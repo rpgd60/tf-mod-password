ff4dabc0fb7e67fae1908d4920ef052fd5c7020c
Rafael Portillo <rpgd60@users.noreply.github.com>
2022-08-25 08:47:13 +0200

    clear info

--- examples/simple_password/main.tf
+++ examples/simple_password/main.tf
@@ -8,6 +8,6 @@ module "a_password" {
 # Works - note that PAT cannot be var or local
   # (works) source = "git::https://<PAT>@dev.azure.com/rpgd60/tf-course-01/_git/tf-modules-ado//modules/tf-password/"
 # source = "git@ssh.dev.azure.com:v3/rpgd60/tf-course-01/tf-modules-ado//modules/tf-password"
-source = "git::https://uvqgvqzf5hqmmdzvngm42hy6mzei5qlyaqedhnexpwy4b4lzefkq@dev.azure.com/rpgd60/tf-course-01/_git/tf-modules-ado//modules/tf-password/"
+source = "git::https://xxxxxlongxxxxxpat@dev.azure.com/rpgd60/tf-course-01/_git/tf-modules-ado//modules/tf-password/"
   length = 12
 }
